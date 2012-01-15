1360378210970e7668ac763aac640fbf2dc026e5
Mohammad Dilshad <dilshadkhan9@gmail.com>
2012-01-15 15:27:25 +0530

    update1

--- index.php
+++ index.php
@@ -147,19 +147,30 @@
 		</div>
 		<div id="nak_middle">
 			<div id="nak_middlecontent">
-
+		<div id="nak_description">
+						<img src="ads/05.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/06.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/07.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/08.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/01.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/02.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/03.jpg" width="595" height="270" alt="" title="" />
+						<img src="ads/04.jpg" width="595" height="270" alt="" title="" />
+					
+			</div>
 			</div>
 			<div id="nak_right">
 		
-		<div id="nak_events" class="nak_sponsers">
-						<img src="ads/01.jpg" width="290" height="300" alt="" title="" />
-						<img src="ads/02.jpg" width="285" height="150" alt="" title="" />
-						<img src="ads/03.jpg" width="285" height="150" alt="" title="" />
-						<img src="ads/04.jpg" width="285" height="150" alt="" title="" />
-						<img src="ads/05.jpg" width="285" height="150" alt="" title="" />
-						<img src="ads/06.jpg" width="285" height="150" alt="" title="" />
-						<img src="ads/07.jpg" width="285" height="150" alt="" title="" />
-						<img src="ads/08.jpg" width="285" height="150" alt="" title="" />
+		<div id="nak_events">
+						<img src="ads/07.jpg" width="290" height="270" alt="" title="" />
+						<img src="ads/08.jpg" width="290" height="270" alt="" title="" />						
+						<img src="ads/01.jpg" width="290" height="270" alt="" title="" />
+						<img src="ads/02.jpg" width="290" height="270" alt="" title="" />
+						<img src="ads/03.jpg" width="290" height="270" alt="" title="" />
+						<img src="ads/04.jpg" width="290" height="270" alt="" title="" />
+						<img src="ads/05.jpg" width="290" height="270" alt="" title="" />
+						<img src="ads/06.jpg" width="290" height="270" alt="" title="" />
+						
 					
 			</div>
 			</div>
@@ -186,6 +197,61 @@
 							container: 'nak_sponsers',
 							width: 285,
 							height: 150,
+							effects: effectsDemo4,
+							display: {
+								autoplay: 3000,
+								autostart: true,
+								first_slide: true,
+								effects_order: 'effects'
+							},
+					});
+				});
+
+$("ul#nak_events").sidebar();
+			$(document).ready(function() {
+						demo4Easing = { name: 'pow2', method: function(x) { return Math.pow(x,2); }};
+						demo4Effect1 = {name: 'myEffect41', rows: 6, cols: 12, fade: true, easing: 'pow2', order: 'cross', delay: 100, duration: 400};
+						demo4Effect2 = {name: 'myEffect42', rows: 6, cols: 12, fade: true, easing: 'pow2', order: 'cross', delay: 100, duration: 400, reverse: true};
+						demo4Effect3 = {name: 'myEffect43', rows: 6, cols: 12, fade: true, easing: 'swing', order: 'rectangle', delay: 200, duration: 1000};
+						demo4Effect4 = {name: 'myEffect44', rows: 6, cols: 12, fade: true, easing: 'swing', order: 'rectangle', delay: 200, duration: 1000, reverse: true};
+						demo4Effect5 = {name: 'myEffect45', rows: 3, cols: 10, zoom: true, move: true, right: true, easing: 'bounce', order: 'circle', delay: 150, duration: 800};
+						demo4Effect6 = {name: 'myEffect46', rows: 3, cols: 10, zoom: true, move: true, left: true, easing: 'bounce', order: 'circle', delay: 150, duration: 800, reverse: true};
+						demo4Effect7 = {name: 'myEffect49', rows: 5, cols: 1, zoom: true, move: true, bottom: true, easing: 'pow2', order: 'circle', delay: 150, duration: 800};
+						demo4Effect8 = {name: 'myEffect410', rows: 5, cols: 1, zoom: true, move: true, top: true, easing: 'pow2', order: 'circle', delay: 150, duration: 800, reverse: true};
+
+						effectsDemo4 = [demo4Effect1,demo4Effect2,demo4Effect3,demo4Effect4,demo4Effect5,demo4Effect6,demo4Effect7,demo4Effect8];
+
+						var demoSlider_4 = Sliderman.slider({
+							container: 'nak_events',
+							width: 290,
+							height: 270,
+							effects: effectsDemo4,
+							display: {
+								autoplay: 3000,
+								autostart: true,
+								first_slide: true,
+								effects_order: 'effects'
+							},
+					});
+				});
+$("ul#nak_description").sidebar();
+			$(document).ready(function() {
+						demo4Easing = { name: 'pow2', method: function(x) { return Math.pow(x,2); }};
+						demo4Effect1 = {name: 'myEffect41', rows: 6, cols: 12, fade: true, easing: 'swing', order: 'cross', delay: 100, duration: 400};
+						demo4Effect2 = {name: 'myEffect42', rows: 6, cols: 12, fade: true, easing: 'swing', order: 'cross', delay: 100, duration: 400, reverse: true};
+						demo4Effect3 = {name: 'myEffect43', rows: 6, cols: 12, fade: true, easing: 'pow2', order: 'rectangle', delay: 200, duration: 1000};
+						demo4Effect4 = {name: 'myEffect44', rows: 6, cols: 12, fade: true, easing: 'pow2', order: 'rectangle', delay: 200, duration: 1000, reverse: true};
+						demo4Effect5 = {name: 'myEffect45', rows: 3, cols: 10, zoom: true, move: true, right: true, easing: 'swing', order: 'circle', delay: 150, duration: 800};
+						demo4Effect6 = {name: 'myEffect46', rows: 3, cols: 10, zoom: true, move: true, left: true, easing: 'swing', order: 'circle', delay: 150, duration: 800, reverse: true};
+						demo4Effect7 = {name: 'myEffect49', rows: 5, cols: 1, zoom: true, move: true, bottom: true, easing: 'bounce', order: 'circle', delay: 150, duration: 800};
+						demo4Effect8 = {name: 'myEffect410', rows: 5, cols: 1, zoom: true, move: true, top: true, easing: 'bounce', order: 'circle', delay: 150, duration: 800, reverse: true};
+
+						effectsDemo4 = [demo4Effect1,demo4Effect2,demo4Effect3,demo4Effect4,demo4Effect5,demo4Effect6,demo4Effect7,demo4Effect8];
+
+						var demoSlider_4 = Sliderman.slider({
+							container: 'nak_description',
+							width: 595,
+							height: 270,
 							effects: effectsDemo4,
 							display: {
 								autoplay: 3000,
